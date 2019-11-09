60eb3d9c52f1e79b5c2afcbb3896bcc668cc163b
Norgg <git@norgg.org>
2019-11-09 15:55:48 +0000

    Placeholder animation

--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -14,11 +14,22 @@ public class Note : MonoBehaviour {
     GameObject core;
     GameObject detection;
 
+    Animator animator;
+
+    public bool broken = false;
+
     void Start() {
         spawner = GameObject.FindObjectOfType<BeatmapParser>();
         text = GetComponentInChildren<TextMeshPro>();
         detection = GameObject.Find("DetectionZone");
         core = GameObject.Find("Core");
+        animator = GetComponentInChildren<Animator>();
+    }
+    
+    public void Break() {
+        animator.Play("destroy");
+        GameObject.Destroy(gameObject, 0.5f);
+        broken = true;
     }
 
     void Update() {
@@ -27,11 +38,9 @@ public class Note : MonoBehaviour {
         directionToSpawner *= speed;
         text.SetText("" + num);
 
-        transform.position += Vector3.down * speed;
-        //transform.position += directionToSpawner;
-
-        if (Vector3.Distance(transform.position, spawner.transform.position) < 0.1f) {
-            Destroy(gameObject);
+        if (!broken) {
+            transform.position += Vector3.down * speed;
         }
+        //transform.position += directionToSpawner;
     }
 }
