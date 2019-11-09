bf0af299b0cf0ae62c6a3a14be949e370a9bfd1b
Jock Findlay <jockfindlay@gmail.com>
2019-11-09 15:45:12 +0000

    Hook up the click event

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,20 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     public void StartGame()
     {
         SceneManager.LoadScene("Instructions");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
