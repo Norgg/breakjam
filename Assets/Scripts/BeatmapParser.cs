f5b9625c65a68f911233fe06bbdf0b1c31b22de1
ZayadNimrod <>
2019-11-09 14:00:07 +0000

    We parse beatmaps now

--- Assets/Scripts/BeatmapParser.cs
+++ Assets/Scripts/BeatmapParser.cs
@@ -10,6 +10,12 @@ public class BeatmapParser : MonoBehaviour {
     public TextAsset file;
     Beatmap map;
 
+    double timer = 0;
+    double timeBetweenBeats;
+    int beat = 0;
+
+    public Note note;
+
     // Start is called before the first frame update
     void Start() {
         string input = file.text;
@@ -18,15 +24,43 @@ public class BeatmapParser : MonoBehaviour {
 
         map = deserializer.Deserialize<Beatmap>(input);
 
-        Debug.Log(map.bpm);
-        Debug.Log(map.beatCodes);
+        timeBetweenBeats = 1.0 / (map.bpm / 60.0);
 
-        
+
+
 
     }
 
     // Update is called once per frame
     void Update() {
+        if (timer >= timeBetweenBeats) {
+            timer -= timeBetweenBeats;
+            SpawnBeat();
+            beat++;
+
+        }
+        timer += Time.deltaTime;
+    }
+
+
+    void SpawnBeat() {
+        Note newNote = GameObject.Instantiate(note);
+        string code = map.beatCodes[beat].code;
+        if (code[0] == 'u') {
+            newNote.dir =NoteSpawner.Direction.Up;
+        }
+        if (code[0] == 'd') {
+            newNote.dir = NoteSpawner.Direction.Down;
+        }
+
+        if (code[0] == 'l') {
+            newNote.dir = NoteSpawner.Direction.Left;
+        }
+        if (code[0] == 'r') {
+            newNote.dir = NoteSpawner.Direction.Right;
+        }
+
+        newNote.num = (int)char.GetNumericValue(code[1]);
 
     }
 }
