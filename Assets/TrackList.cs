258c05023284a97cabd203610ff3d4dbf94816ea
Jock Findlay <jockfindlay@gmail.com>
2019-11-09 16:32:39 +0000

    Select Track wip

---
+++ Assets/TrackList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackList : MonoBehaviour
+{
+    string[] trackNames =
+    {
+        "Track 1",
+        "Track 2",
+        "Track 3",
+    };
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach (var title in trackNames)
+        {
+            //var button = new Button
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
