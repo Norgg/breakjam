cdb8dfacd1575a1d1f7277703b084120682dad9f
Jock Findlay <jockfindlay@gmail.com>
2019-11-10 15:25:31 +0000

    Key-pressed feedback

---
+++ Assets/Scripts/FeedbackF1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackF1 : MonoBehaviour
+{
+    public GameObject F1Key;
+    GameFeedbacker feedbacker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        feedbacker = new GameFeedbacker("Fret1", F1Key);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        feedbacker.Update();
+    }
+}
---
+++ Assets/Scripts/GameFeedbacker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+
+public class GameFeedbacker
+{
+    private readonly string key;
+    private readonly GameObject gameObject;
+
+    public GameFeedbacker(string key, GameObject gameObject)
+    {
+        this.key = key;
+        this.gameObject = gameObject;
+    }
+
+    public void Update()
+    {
+        if (Input.GetButton(key))
+        {
+            gameObject.GetComponent<Renderer>().enabled = true;
+        }
+        else
+        {
+            gameObject.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
